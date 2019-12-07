6e8a525813f2eb20a8931bfc45141c3a8c41db2a
azizahmed95 <aziz3140@gmail.com>
2019-12-08 00:02:43 +0530

    first commit of git in master branch

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Demo Page</title>
+</head>
+<body>
+	<h1>Demo Project</h1>
+	<p>This is just a demo site under construction....</p>
+</body>
+</html>
+<!-- https://github.com/azizahmed95/gitDemo.git -->
