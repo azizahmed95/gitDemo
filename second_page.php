c47ed696d14fd5e8a17870efd68c2b88c31ab7e6
azizahmed95 <aziz3140@gmail.com>
2019-12-08 13:20:52 +0530

    adding second page

---
+++ second_page.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Second Page</title>
+</head>
+<body>
+	<h1>Second File</h1>
+
+	<div id="header">
+		<p>This is the header of the web page
+	</div>
+
+	<p>This is second page of website.</p>
+</body>
+</html>
